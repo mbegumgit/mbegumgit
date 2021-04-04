76a08d81e3a88992ec239981233eb4a9ae6c073c
unknown <you@example.com>
2021-04-04 23:10:45 +0530

    added tblupload

--- spellbee/urls.py
+++ spellbee/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("<int:beeword_id>", views.beeword_id, name="score_view"),
     path("test",views.index,name="index"),
     path('bee/<int:beeword_key>',views.beeword,name='beeword'),
-    
+    path("scoretbupdate2021", views.scoretb_update, name="tblupdate"),
     path("score", views.scoretot, name="scoretot"),
     path("scoreboard", views.scoreboard, name="scoreboard")
     
