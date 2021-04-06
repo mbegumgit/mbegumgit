7efa87730b6ab327be6d37ced9aea6ded975a550
unknown <you@example.com>
2021-04-06 16:22:42 +0530

    static root configured

--- Bee_Project/settings.py
+++ Bee_Project/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
  
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 django_heroku.settings(locals())
