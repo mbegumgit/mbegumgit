60091bd939bc4f09ecd1b683e3483ca1876a5c86
unknown <you@example.com>
2021-04-06 17:34:30 +0530

    key modified in Settings

--- Bee_Project/settings.py
+++ Bee_Project/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7a-r#hx)o)*998zcg*1mym(2gh7+9qfvk4pmw(n_!0+5#qg$+q'
+#SECRET_KEY = '7a-r#hx)o)*998zcg*1mym(2gh7+9qfvk4pmw(n_!0+5#qg$+q'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['spellbeeword.herokuapp.com']
 
 
 # Application definition
