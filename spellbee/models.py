b3ca5970ae9fdaa348bfb6f01a3c4bcc62fda814
unknown <you@example.com>
2021-04-09 21:25:32 +0530

    removed unique option in Score table

--- spellbee/models.py
+++ spellbee/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+#from django.contrib.auth.models import User
 
 # Create your models here.
 class BeeWord(models.Model):
@@ -23,17 +24,18 @@ class CustomBooleanField(models.BooleanField):
         return int(value) # return 0/1
 
 class Score(models.Model):
-    pickword = models.ForeignKey(BeeWord,unique=True, on_delete=models.CASCADE, related_name="wordscore")
+    pickword = models.ForeignKey(BeeWord, on_delete=models.CASCADE, related_name="wordscore")
     pickidx = models.IntegerField(default=0)
     lastscore = models.BooleanField(default=False)
     scoreboard  = models.IntegerField(default=0)
+    
     
 
     class Meta:
         db_table = 'Spellbeeword_Score_tb'       
         managed = True
     def __str__(self):
-        score_string = f"Index -{self.id}: {self.pickword} -- Lastscore is {self.lastscore} & attempted {self.pickidx} times"
+        score_string = f"Index -{self.id}  {self.pickword} -- Lastscore is {self.lastscore} & attempted {self.pickidx} times"
         return score_string
 
 class YearList(models.Model):
