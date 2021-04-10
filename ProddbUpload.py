043cdf8147c09bb768996b00a4f69789aed36cc1
unknown <you@example.com>
2021-04-10 15:33:05 +0530

    selecct query

--- ProddbUpload.py
+++ ProddbUpload.py
@@ -3,6 +3,16 @@ import psycopg2
 
 def writedb(conn):
     cur = conn.cursor()
+    cur.execute('SELECT version()')
+     
+
+    # display the PostgreSQL database server version
+    db_version = cur.fetchone()
+    print(db_version)
+    cur.execute('SELECT * FROM Spellbeeword_tb')
+    first= cur.fetchone()
+
+    print('first row:' first)
     try:
         with  open("spellbee/docs/Spell_Bee_Word_db.csv", 'r') as f:
             cur.copy_from(f,'Spellbeeword_tb',sep=',')
@@ -23,7 +33,9 @@ def main():
 
     try:
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
+        # execute an SQL statement to get the HerokuPostgres database version
+        print('PostgreSQL database version:')
+        
         writedb(conn)
 
     except(Exception, psycopg2.DatabaseError) as error:
