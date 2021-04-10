99e74ce71ed8f14f78d1127f12aeef11a9754f38
unknown <you@example.com>
2021-04-10 11:53:10 +0530

    added Beeword csv file and Postgres-db upload file

--- ProddbUpload.py
+++ ProddbUpload.py
@@ -1,28 +1,36 @@
-import os, csv
+import os
 import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-
-try:
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    
+def writedb(conn):
     cur = conn.cursor()
-    inp_file = open("spellbee/docs/Spell_Bee_Merged_db.csv")
-    rows = csv.reader(inp_file)
-    head =True
-    for row in rows:
-        if not head:
-            data = (row[0],row[1],row[2])   
-            cur.execute("INSERT INTO Spellbeeword_tb VALUES(?, ?,?)", data)
-        else:
-            head = False
-    cur.execute("SELECT * FROM Spellbeeword_tb limit 5")
-    print(cur.fetchall())   
-    conn.commit()
-except(Exception, psycopg2.DatabaseError) as error:
-    print(error)
-finally:
-    if conn:
-        conn.close()
-    inp_file.close()
+    try:
+        with  open("spellbee/docs/Spell_Bee_Word_db.csv", 'r') as f:
+            cur.copy_from(f,'Spellbeeword_tb',sep=',')
+            # commit changes
+            conn.commit()
+    
+    except Exception as error:
+        print(error)
+    finally:
+        if conn:
+            conn.close()
+            f.close()
+
+def main():
+
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+
+    try:
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+        writedb(conn)
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn:
+            conn.close()\
+if __name__ == "__main__":
+    main()
+        
