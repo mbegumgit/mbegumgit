d6f0c1af2b654c67964c7cd772240d70b94aa2f8
unknown <you@example.com>
2021-04-11 09:10:56 +0530

    testing

--- ProddbUpload.py
+++ ProddbUpload.py
@@ -9,10 +9,17 @@ def writedb(conn):
     # display the PostgreSQL database server version
     db_version = cur.fetchone()
     print(db_version)
-    cur.execute('SELECT * FROM Spellbeeword_tb')
+    cur.execute('SELECT * FROM information_schema.tables;')
+    first = cur.fetchall()
+    for tb in first:
+        
+        print('table row:',tb)
+        
+    cur.execute('SELECT * FROM Spellbeeword_tb;')
     first= cur.fetchone()
 
     print('first row:', first)
+
     try:
         with  open("spellbee/docs/Spell_Bee_Word_db.csv", 'r') as f:
             cur.copy_from(f,'Spellbeeword_tb',sep=',')
@@ -30,6 +37,7 @@ def main():
 
     DATABASE_URL = os.environ['DATABASE_URL']
 
+    print('Printing postgresql db info:',DATABASE_URL)
 
     try:
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
