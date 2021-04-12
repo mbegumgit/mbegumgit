b6a66c2e460957b85b33071235ecb3bd21a3a804
unknown <you@example.com>
2021-04-12 16:41:01 +0530

    Unique csv file update

--- ProddbUpload.py
+++ ProddbUpload.py
@@ -6,8 +6,9 @@ def writedb(conn):
     print("Started opening file to copy into table")
 
     try:
-        with  open("spellbee/docs/Spell_Bee_Word_db.csv", 'r', encoding="utf-8") as f:
-            sql_copy= "COPY spellbeeword_tb  FROM stdin (FORMAT CSV)"
+        with  open("spellbee/docs/Spell_Bee_Word_Unique_db.csv", 'r', encoding="utf-8") as f:
+ 
+            sql_copy ="COPY spellbeeword_tb  FROM STDIN WITH CSV HEADER DELIMITER ',' "
             cur.copy_expert(sql=sql_copy,file=f)
             print('successfully copied files into db')
             # commit changes
@@ -19,25 +20,33 @@ def writedb(conn):
     finally:
         if conn:
             conn.close()
+            print("Finally connection closed")
             f.close()
 
 def main():
 
     DATABASE_URL = os.environ['DATABASE_URL']
-
+    """ DATABASES = {
+        'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'spellbee_test_db',
+       'USER' : 'spellbee',
+       'PASSWORD': 'spellbee@123',
+       'HOST': 'localhost',
+       'PORT': '',
+   }
+} """
     
 
     try:
-        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        conn = psycopg2.connect(DATABASE_URL)
         
         
         writedb(conn)
 
     except(Exception, psycopg2.DatabaseError) as error:
         print(error)
-    finally:
-        if conn:
-            conn.close()
+    
 
 if __name__ == "__main__" :
     main()
